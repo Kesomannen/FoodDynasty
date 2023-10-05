7c43f94e78d548f35840bae3e0fb90344d35ad82
Kesomannen <bobbo.nidor@gmail.com>
2023-10-05 18:44:35 +0200

    Split Conveyor now works

---
+++ Assets/Dynasty/Machines/Components/SplitConveyor.cs
@@ -0,0 +1,19 @@
+﻿using Dynasty.Food.Instance;
+using UnityEngine;
+
+namespace Dynasty.Machines {
+
+public class SplitConveyor : FoodMachineComponent {
+    [SerializeField] Conveyor[] _conveyors;
+    
+    int _index;
+    
+    protected override void OnTriggered(FoodBehaviour food) {
+        _index = (_index + 1) % _conveyors.Length;
+        for (var i = 0; i < _conveyors.Length; i++) {
+            _conveyors[i].enabled = i == _index;
+        }
+    }
+}
+
+}
